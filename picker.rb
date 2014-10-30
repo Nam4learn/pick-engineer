107d0a071b12aa5ffa97235d9f8ea5a2c0b4588c
Namchai Nimitjitpukdee <nam4learn@gmail.com>
2014-10-30 09:05:50 +0700

    Edit picker.rb file

--- picker.rb
+++ picker.rb
@@ -1,10 +1,6 @@
-=begin
-  function pick_engineer use for random engineer name from text file
-  this case text file is engineers.txt
-=end
 
 def pick_engineer
-  name = [] # create list for store all name in text file
+  name = [] # list store all name
   File.open("engineers.txt").each { |n| name.push(n) } # open text file and push all name to list
   name[rand(name.length)] # return name in text file which random
 end
